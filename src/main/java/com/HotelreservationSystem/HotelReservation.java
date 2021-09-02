fe7e404a09f3e3ea7b17e3800ef652ca7f9f58d2
sheetal-10 <sheetalp257@gmail.com>
2021-09-02 08:30:02 +0530

    Added: welcome message to hotel reservation system

--- src/main/java/com/HotelreservationSystem/HotelReservation.java
+++ src/main/java/com/HotelreservationSystem/HotelReservation.java
@@ -1,5 +1,7 @@
 package com.HotelreservationSystem;
 
 public class HotelReservation {
-
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation System");
+	}
 }
---
+++ src/test/java/com/HotelReservationSystemTestCase/HotelReservationSystemTest.java
@@ -0,0 +1,5 @@
+package com.HotelReservationSystemTestCase;
+
+public class HotelReservationSystemTest {
+
+}
